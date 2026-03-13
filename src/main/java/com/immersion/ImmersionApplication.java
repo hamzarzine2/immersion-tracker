df3da2e3f640e69efa5170cfbc0560a74939fd77
hamzarzine2 <hamza.rzine@outlook.com>
2026-03-13 21:37:59 +0100

    init repo

---
+++ src/main/java/com/immersion/ImmersionApplication.java
@@ -0,0 +1,13 @@
+package com.immersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/immersion/ImmersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immersion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
