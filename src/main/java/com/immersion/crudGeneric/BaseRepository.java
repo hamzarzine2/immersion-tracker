147c7e57fb4fbf206812870c14c8c17a3aa7cb81
hamzarzine2 <hamza.rzine@outlook.com>
2026-03-14 00:20:16 +0100

    Feat: Add Crud generic + user Crud test

---
+++ src/main/java/com/immersion/crudGeneric/BaseRepository.java
@@ -0,0 +1,8 @@
+package com.immersion.crudGeneric;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T, U> extends JpaRepository<T, U> {
+}
